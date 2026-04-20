8393f4d2394c9d78d27fcd8c05b1f51c56558460
Xorrim <yariklastov29@gmail.com>
2026-04-20 16:20:01 +0300

    fixed inaccuracies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const data = fs.readFileSync('./article.txt', 'utf8');
+// отримуємо шлях до поточного файлу
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// формуємо правильний шлях
+const filePath = path.join(__dirname, 'article.txt');
+
+const data = fs.readFileSync(filePath);
 
 const charCount = data.length;
 
--- src/task3.js
+++ src/task3.js
@@ -1,12 +1,19 @@
 import fs from 'fs';
 function changeFile(fileName, replaced, changing) {
-   fs.readFile(fileName, 'utf8', (err,data) => {
-      if (err) {
-        console.error(err)
-        return;
+  fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
     }
     const change = data.replaceAll(replaced, changing);
-    const info = fs.writeFileSync(fileName, change);
-   });
+    fs.writeFile(fileName, change, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('File updated');
+    });
+  });
 }
-changeFile('./src/log.txt', 'Script', 'Code'); 
+
+changeFile('./src/log.txt', 'Script', 'Code');
